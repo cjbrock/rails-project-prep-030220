0a290236fa9470faee243bf8908dc2470e303c66
cjbrock <corinna@flatironschool.com>
2020-05-21 13:50:06 -0400

    post study group code

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'authorized', to: 'sessions#page_requires_login'
+
+  root to: 'sessions#welcome'
+end
